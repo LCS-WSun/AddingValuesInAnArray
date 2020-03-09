4e79e20c83dde7c8228e64e5234d2d3fdbb8a737
Wesley Sun <wsun@lcs.on.ca>
2020-03-09 01:18:18 -0400

    Initial Commit

---
+++ AddingValuesInAnArray/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AddingValuesInAnArray
+//
+//  Created by Sun, Wesley on 2020-03-09.
+//  Copyright © 2020 Wesley Sun. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
